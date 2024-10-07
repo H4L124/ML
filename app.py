33746194e3da0e077a1e7ec793381f44d76217d8
Nur Haliza Rositasari <97608900+H4L124@users.noreply.github.com>
2024-10-07 08:27:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import shap
 import matplotlib.pyplot as plt
 
 # Fungsi untuk load CSS dari file eksternal
-def local_css(style.css):
-    with open(style.css) as f:
+def local_css("style.css"):
+    with open("style.css") as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 # Panggil fungsi untuk memuat CSS
